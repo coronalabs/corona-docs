2232b9394014ba6a4a737b75fe03505b58395e1d
Serkan Aksit <5222368+sekodev@users.noreply.github.com>
2023-03-22 15:23:44 +0300

    Bump version number

--- bin/build.lua
+++ bin/build.lua
@@ -66,7 +66,7 @@ local output_images_dir = html_dir .. "/images"
 
 -- strings to substitute throughout docs content (not headers or footers though)
 local title_prefix = "Solar2D Documentation"
-local default_rev_label = "Release 2022.3683"
+local default_rev_label = "Release 2023.3686"
 local REV_LABEL = default_rev_label
 
 local CORONA_CORE_PRODUCT = "Solar2D"
